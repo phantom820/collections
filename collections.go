214123f194368042a96de8c23d2b556a600aaf98
phantom820 <nkambule773@gmail.com>
2022-05-11 21:10:40 +0200

    Added default package in root.

---
+++ collections.go
@@ -0,0 +1,10 @@
+// Package collections provides implementation of some common data structures. The data strucures implemented are
+// Red Black Tree - A node stores a key and it associate value.
+// HashMap - A map in which the underlying container in individual buckets is a Red Black Tree.
+// List - A doubly linked list.
+// ForwardList - A singly linked list.
+// ListQueue - A ForwardList based implementation of a queue.
+// SliceQueue - A slice based implementatioin of a queue.
+// ListStack - A ForwardList based implementation of a stack.
+// SliceQueue - A slice based implementation of a queue.
+package collections

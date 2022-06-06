82852db68e8693376c2b2c526316286c8c290eea
phantom820 <nkambule773@gmail.com>
2022-06-07 01:27:43 +0200

    Made add front for queues consistent.

--- queues/queues.go
+++ queues/queues.go
@@ -24,9 +24,9 @@ type Queue[T types.Equitable[T]] interface {
 // Dequeue an interface that a dequeue implementation should satisfy.
 type Dequeue[T types.Equitable[T]] interface {
 	collections.Collection[T]
-	AddFront(elements ...T) // Adds elements to the front of the queue.
-	Front() T               //  Returns the front element of the queue. Will panic if no front element.
-	RemoveFront() T         // Returns and removes the front element of the queue. Will panic if no front element.
-	Back() T                //  Returns the back element of the queue. Will panic if no back element.
-	RemoveBack() T          //  Returns and removes the item at the back of the queue
+	AddFront(elements ...T) bool // Adds elements to the front of the queue.
+	Front() T                    //  Returns the front element of the queue. Will panic if no front element.
+	RemoveFront() T              // Returns and removes the front element of the queue. Will panic if no front element.
+	Back() T                     //  Returns the back element of the queue. Will panic if no back element.
+	RemoveBack() T               //  Returns and removes the item at the back of the queue
 }
